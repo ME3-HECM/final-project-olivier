8c7b16de96c1b0b19ceda3213057fa6fbac03468
wisdomokoro1232 <122476723+wisdomokoro1232@users.noreply.github.com>
2023-02-28 11:17:30 +0000

    Adding necessary include fucntions and setup functions in main

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
-/*
- * File:   main.c
- * Author: Windows
- *
- * Created on 27 February 2023, 20:50
- */
-
+#pragma config FEXTOSC = HS     // External Oscillator mode Selection bits (HS (crystal oscillator) above 8 MHz; PFM set to high power)
+#pragma config RSTOSC = EXTOSC_4PLL// Power-up default value for COSC bits (EXTOSC with 4x PLL, with EXTOSC operating per FEXTOSC bits)
+#pragma config WDTE = OFF        // WDT operating mode (WDT enabled regardless of sleep)
 
 #include <xc.h>
+#include "serial.h"
+#include <stdio.h>
+#include "color.h"
+#include "serial.h"
+
+#define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
 
 void main(void) {
-    return;
+    char data[20];
+    initUSART4();
+    I2C_2_Master_Init;
+    while (1){ 
+}
 }
